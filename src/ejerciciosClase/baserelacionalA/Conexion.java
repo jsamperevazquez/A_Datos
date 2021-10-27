33c6617d75e28db42c9412eda8e7da66933e07a7
JSampereVázquez <samperevazquez@danielcastelao.org>
2021-10-28 01:23:00 +0200

    Creado el método para realizar consultas a tabla produtos

--- src/ejerciciosClase/baserelacionalA/Conexion.java
+++ src/ejerciciosClase/baserelacionalA/Conexion.java
@@ -13,16 +13,18 @@ public class Conexion {
     String sid = "postgres";
     String usuario = "oracle";
     String password = "oracle";
-    String url = driver + host+ porto + "/" + sid;
-    Connection conn = DriverManager.getConnection(url,usuario,password);
+    String url = driver + host + porto + "/" + sid;
+    Connection conn = DriverManager.getConnection(url, usuario, password);
+
     public Conexion() throws SQLException {
     }
 
-
+    // Método de conexión que devuelve un objeto de tipo Connection para usar con los métodos a base de datos
     public Connection conexion() throws SQLException {
 
-      return conn;
+        return conn;
     }
+
     public void insertarProducto(String codigo, String nombre, float precio, String fechaCaducidad) throws SQLException {
         // Creo un objeto de tipo SimpleDateFormat para poder asignar el formato deseado
         SimpleDateFormat df = new SimpleDateFormat("dd/MM/yyyy");
@@ -36,10 +38,26 @@ public class Conexion {
         java.sql.Date fechaSQL = new java.sql.Date(fecha.getTime()); // Creo una fecha de SQL que lo requiere el PreparedStatement y le paso como parametro el date.getTime()
 
         PreparedStatement insertar = conn.prepareStatement("INSERT INTO produtos values (?,?,?,?)");
-        insertar.setString(1,codigo);
-        insertar.setString(2,nombre);
-        insertar.setFloat(3,precio);
-        insertar.setDate(4,fechaSQL);
+        // Le paso al insert cada parámetro que quiero insertar en el index que corresponde:
+        insertar.setString(1, codigo);
+        insertar.setString(2, nombre);
+        insertar.setFloat(3, precio);
+        insertar.setDate(4, fechaSQL);
         insertar.execute();
+    }
+
+    public void listaProductos() throws SQLException {
+        Statement consulta = conn.createStatement(); // Creo un objeto de tipo Statement y le asigno el método createStatement del objeto de conexion
+        ResultSet r = consulta.executeQuery("SELECT * from produtos"); // Creo un objeto de tipo Resultset y le asigno la query que preciso
+        int fila = 1;
+        while (r.next()) {
+            // Recojo los valores de cada columna de la tabla en función de su index:
+            String codigo = r.getString(1);
+            String nombre = r.getString(2);
+            float prezo = r.getFloat(3);
+            String fechaCaducidad = r.getDate(4).toString(); // Por simplicidad, la paso a String
+            System.out.println("Fila " + fila + ": " + codigo + " " + nombre + " " + prezo + " " + fechaCaducidad);
+            fila++;
+        }
     }
 }
--- src/ejerciciosClase/baserelacionalA/baseRelacional.java
+++ src/ejerciciosClase/baserelacionalA/baseRelacional.java
@@ -9,9 +9,13 @@ public class baseRelacional {
         try {
             Conexion con = new Conexion();
             con.conexion();
+            /*
             con.insertarProducto("p1","parafusos",3.0F, "27/12/2020");
             con.insertarProducto("p2","cravos",4.0F, "06/04/2020");
             con.insertarProducto("p3","tachas",3.0F, "03/07/2020");
+
+             */
+            con.listaProductos();
         } catch (SQLException e) {
             System.out.println("Error en la conexión" + e.getMessage());
         }
