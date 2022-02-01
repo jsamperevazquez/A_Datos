1d390d899cca9ba69fe65149e99aa7ce8badb0b3
JSampereVázquez <samperevazquez@danielcastelao.org>
2022-02-01 12:34:52 +0100

    Nuevos métodos agregados

--- src/mongo/conexion/MongoConexion.java
+++ src/mongo/conexion/MongoConexion.java
@@ -2,7 +2,9 @@ package mongo.conexion;
 
 import com.mongodb.BasicDBObject;
 import com.mongodb.MongoClient;
+import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.model.Projections;
 import org.bson.Document;
@@ -46,7 +48,44 @@ public class MongoConexion {
     //Método para listar documentos
     public void consultarPorId(ObjectId objectid){
         BasicDBObject condition = new BasicDBObject("_id",objectid);
+        // projection son los campos que quiero recuperar:
         Document document = coleccion.find(condition).projection(Projections.include("_id","kind","score")).first();
+        //Recojo los valores
+        ObjectId objId = document.getObjectId("_id");
+        String kind = document.getString("kind");
+        Double score = document.getDouble("score");
+        System.out.println("id: " + objId + " kind: " + kind + " score: " + score);
+    }
+    public void otraManeraConsultaPorID(ObjectId objectId){
+        BasicDBObject condition = new BasicDBObject("_id",objectId);
+        BasicDBObject campos = new BasicDBObject();
+        campos.put("kind",1);
+        campos.put("score",1);
+        Document document = coleccion.find(condition).projection(campos).first();
+        ObjectId objId = document.getObjectId("_id");
+        String kind = document.getString("kind");
+        Double score = document.getDouble("score");
+        System.out.println("id: " + objId + " kind: " + kind + " score: " + score);
+    }
 
+    public void consultarCampoValor(String campo,Object valor){
+        BasicDBObject condition = new BasicDBObject(campo,valor);
+        FindIterable<Document> docs = coleccion.find(condition);
+        MongoCursor<Document> cursor = docs.iterator();
+        while (cursor.hasNext()){
+            Document doc = cursor.next();
+            ObjectId objId = doc.getObjectId("_id");
+            String kind = doc.getString("kind");
+            Double score = doc.getDouble("score");
+            Double student = doc.getDouble("student");
+            System.out.println("id: " + objId + " kind: " + kind + " score: " + score + " student: " + student);
+        }
+    }
+    public void actualizarPorID(ObjectId id,String campo, Object valor){
+        coleccion.updateOne(new BasicDBObject("_id",id),new BasicDBObject("$set",new BasicDBObject(campo,valor)));
+    }
+
+    public void incrementarCamposNumericos(ObjectId id,String campo, Object valor){
+        coleccion.updateOne(new BasicDBObject("_id",id),new BasicDBObject("$inc",new BasicDBObject(campo,valor)));
     }
 }
--- src/mongo/conexion/Principal.java
+++ src/mongo/conexion/Principal.java
@@ -4,9 +4,18 @@ package mongo.conexion;
 import com.mongodb.BasicDBObject;
 import mongo.conexion.MongoConexion;
 import org.bson.Document;
+import org.bson.types.ObjectId;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Principal {
     public static void main(String[] args) {
+        // Desactivar logs de mongo
+        Logger logger = Logger.getLogger("org.mongodb.driver");
+        logger.setLevel(Level.SEVERE);
+
+
         Document document = new Document("kind","taller")
                 .append("score", 61.0)
                 .append("enderezo", new Document()
@@ -17,13 +26,20 @@ public class Principal {
         BasicDBObject campo = new BasicDBObject("kind","taller");
         BasicDBObject set = new BasicDBObject("$set",new BasicDBObject("score",98.0));
         BasicDBObject borrar = new BasicDBObject("kind", "taller");
+        ObjectId id = new ObjectId("4c90f2543d937c033f424798");
+        ObjectId id2 = new ObjectId("4c90f2543d937c033f424985");
         MongoConexion mongo = new MongoConexion();
         mongo.conectar_servidor();
         mongo.conectar_base("training");
         mongo.apuntar_coleccion("scores");
         //mongo.insertarEnDocu(document);
         //mongo.updateDocu(campo,set);
-        mongo.deleteDocu(borrar);
+        //mongo.consultarPorId(id2);
+        //mongo.otraManeraConsultaPorID(id2);
+        //mongo.consultarCampoValor("student",99.0);
+        //mongo.deleteDocu(borrar);
+        //mongo.actualizarPorID(id2,"score",75.0);
+        //mongo.incrementarCamposNumericos(id2,"score",5.0);
         mongo.conectar_servidor().close();
 
 
